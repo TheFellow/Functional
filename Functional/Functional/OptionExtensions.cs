25449135957df8adf8a6ea577adb6f6bed4520f5
Ryan Harris <thomasryanharris@gmail.com>
2019-10-13 16:10:52 -0700

    Added first set of extensions: NoneIfNull, and Reduce

---
+++ Functional/Functional/OptionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional
+{
+    public static class OptionExtensions
+    {
+        public static Option<T> NoneIfNull<T>(this T obj) =>
+            !(obj is null)
+                ? (Option<T>)new Some<T>(obj)
+                : new None<T>();
+
+        #region Reduce
+
+        public static T Reduce<T>(this Option<T> option, T whenNone) =>
+            option is Some<T> some
+                ? some.Content
+                : whenNone;
+
+        public static T Reduce<T>(this Option<T> option, Func<T> whenNone) =>
+            option is Some<T> some
+                ? some.Content
+                : whenNone();
+
+        #endregion
+    }
+}
---
+++ Functional/FunctionalTests/OptionExtensionsTests.cs
@@ -0,0 +1,95 @@
+﻿using Functional;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalTests
+{
+    [TestClass]
+    public class OptionExtensionsTests
+    {
+        private const string _whenNone = "Replacement";
+        private const string _sample = "Hello World";
+
+        [TestMethod]
+        public void NoneIfNull_OnNullInstance_ReturnsNoneOfThatType()
+        {
+            string str = null!;
+
+            var result = str.NoneIfNull();
+
+            Assert.IsInstanceOfType(result, typeof(None<string>));
+        }
+
+        [TestMethod]
+        public void NoneIfNull_OnNonNullInstance_ReturnsSomeOfThatType()
+        {
+            string str = "";
+
+            var result = str.NoneIfNull();
+
+            Assert.IsInstanceOfType(result, typeof(Some<string>));
+        }
+
+        #region Reduce Tests
+
+        [TestMethod]
+        public void ReduceWithReplacement_SomeOfT_ReturnsContent()
+        {
+            Option<string> some = _sample;
+
+            string result = some.Reduce(_whenNone);
+
+            Assert.AreEqual(_sample, result);
+        }
+
+        [TestMethod]
+        public void ReduceWithFunc_OnSomeOfT_ReturnsContentWithoutCallingFunc()
+        {
+            Option<string> some = _sample;
+
+            bool calledFunc = false;
+
+            string result = some.Reduce(() =>
+            {
+                calledFunc = true;
+                return _whenNone;
+            });
+
+            Assert.IsFalse(calledFunc);
+            Assert.AreEqual(_sample, result);
+        }
+
+        [TestMethod]
+        public void ReduceWithReplacement_NoneOfT_ReturnsReplacement()
+        {
+            Option<string> none = None.Value;
+
+            string result = none.Reduce(_whenNone);
+
+            Assert.AreEqual(_whenNone, result);
+        }
+
+        [TestMethod]
+        public void ReduceWithFunc_OnNoneOfT_CallsFuncForReplacement()
+        {
+            Option<string> none = None.Value;
+
+            bool calledFunc = false;
+
+            string result = none.Reduce(() =>
+            {
+                calledFunc = true;
+                return _whenNone;
+            });
+
+            Assert.IsTrue(calledFunc);
+            Assert.AreEqual(_whenNone, result);
+        }
+
+        #endregion
+    }
+}
