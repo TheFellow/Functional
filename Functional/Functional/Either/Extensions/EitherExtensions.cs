4730afc2cdb44c2c04adfb393e6201fe93ff44ed
Ryan Harris <thomasryanharris@gmail.com>
2019-10-18 23:36:28 -0700

    Add EitherExtensions.Reduce

---
+++ Functional/Functional/Either/Extensions/EitherExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Functional
+{
+    public static class EitherExtensions
+    {
+        public static TRight Reduce<TLeft, TRight>(this Either<TLeft, TRight> either, TRight whenLeft) =>
+            either is Right<TLeft, TRight> right ? right.Content : whenLeft;
+
+
+    }
+}
---
+++ Functional/FunctionalTests/Either/ExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using Functional;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FunctionalTests.Either
+{
+    [TestClass]
+    public class ExtensionsTests
+    {
+        [TestMethod]
+        public void Reduce_WhenRight_ReturnsRight()
+        {
+            Either<int, string> either = "Hello World";
+
+            var result = either.Reduce("Replacement");
+
+            Assert.AreEqual("Hello World", result);
+        }
+
+        [TestMethod]
+        public void Reduce_WhenLeft_ReturnsReplacement()
+        {
+            Either<int, string> either = 4;
+
+            var result = either.Reduce("Replacement");
+
+            Assert.AreEqual("Replacement", result);
+        }
+    }
+}
