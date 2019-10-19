c8bbdbcfbc9a7fad8cce2042ca2ca07624dde8db
Ryan Harris <thomasryanharris@gmail.com>
2019-10-18 22:15:32 -0700

    EnumerableExtensions.FirstOrNone

--- Functional/Functional/Extensions/EnumerableExtensions.cs
+++ Functional/Functional/Extensions/EnumerableExtensions.cs
@@ -10,5 +10,16 @@ namespace Functional
             sequence.Select(map)
                 .OfType<Some<TResult>>()
                 .Select(s => s.Content);
+
+        public static Option<T> FirstOrNone<T>(this IEnumerable<T> sequence) =>
+            sequence
+                .Select(o => (Option<T>)new Some<T>(o))
+                .DefaultIfEmpty(None.Value)
+                .First();
+
+        public static Option<T> FirstOrNone<T>(this IEnumerable<T> sequence, Func<T, bool> predicate) =>
+            sequence
+                .Where(predicate)
+                .FirstOrNone();
     }
 }
--- Functional/FunctionalTests/Option/EnumerableExtensionsTests.cs
+++ Functional/FunctionalTests/Option/EnumerableExtensionsTests.cs
@@ -20,5 +20,52 @@ namespace FunctionalTests.Option
 
             Assert.IsTrue(result.SequenceEqual(new string[] { "0", "2", "4" }));
         }
+
+        [TestMethod]
+        public void FirstOrNone_OnEmptySequence_IsNone()
+        {
+            var result = Enumerable.Empty<int>().FirstOrNone();
+
+            Assert.IsInstanceOfType(result, typeof(None<int>));
+        }
+
+        [TestMethod]
+        public void FirstOrNone_OnNonEmptySequence_IsFirstEntry()
+        {
+            var result = Enumerable.Range(4, 8).FirstOrNone();
+
+            if (result is Some<int> someInt)
+            {
+                Assert.AreEqual(4, someInt.Content);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void FirstOrNone_WithTruePredicate_IsSome()
+        {
+            var result = Enumerable.Range(1, 6).FirstOrNone(i => i % 2 == 0);
+
+            if (result is Some<int> someInt)
+            {
+                Assert.AreEqual(2, someInt.Content);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+
+        }
+
+        [TestMethod]
+        public void FirstOrNone_WithContradiction_IsNone()
+        {
+            var result = Enumerable.Range(1, 6).FirstOrNone(i => i < 1);
+
+            Assert.IsInstanceOfType(result, typeof(None<int>));
+        }
     }
 }
