dd1ead97245d2dc069e4fdd55328104ce4791300
Ryan Harris <thomasryanharris@gmail.com>
2019-10-18 21:46:59 -0700

    Moved Extensions into their own folder (same ns), Added EnumerableExtensions

---
+++ Functional/Functional/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Functional
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<TResult> Flatten<T, TResult>(this IEnumerable<T> sequence, Func<T, Option<TResult>> map) =>
+            sequence.Select(map)
+                .OfType<Some<TResult>>()
+                .Select(s => s.Content);
+    }
+}
---
+++ Functional/FunctionalTests/Option/EnumerableExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using Functional;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace FunctionalTests.Option
+{
+    [TestClass]
+    public class EnumerableExtensionsTests
+    {
+        [TestMethod]
+        public void Flatten_ReturnsEntriesMatchingMap()
+        {
+            var values = Enumerable.Range(0, 6);
+
+            var result = values.Flatten(
+                i => i % 2 == 0
+                    ? (Option<string>)new Some<string>(i.ToString())
+                    : None.Value);
+
+            Assert.IsTrue(result.SequenceEqual(new string[] { "0", "2", "4" }));
+        }
+    }
+}
