2c34fffaf30398962d36653faec7bb700e9d4b0e
Ryan Harris <thomasryanharris@gmail.com>
2019-10-18 22:45:53 -0700

    Fix nullability warning for IDictionary<TKey,> related to TKey

--- Functional/Functional/Extensions/DictionaryExtensions.cs
+++ Functional/Functional/Extensions/DictionaryExtensions.cs
@@ -4,7 +4,8 @@ namespace Functional
 {
     public static class DictionaryExtensions
     {
-        public static Option<TValue> TryGetValue<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key) =>
+        public static Option<TValue> TryGetValue<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key)
+            where TKey : notnull =>
             dict.TryGetValue(key, out TValue value)
                 ? (Option<TValue>)new Some<TValue>(value)
                 : None.Value;
