909806980dfbd7c52a13a71529b6e4a0c005d5b1
Ryan Harris <thomasryanharris@gmail.com>
2019-10-28 23:27:03 -0700

    Either.Map for one-track function

--- Functional/Functional/Either/Extensions/EitherExtensions.cs
+++ Functional/Functional/Either/Extensions/EitherExtensions.cs
@@ -9,5 +9,10 @@ namespace Functional
 
         public static TRight Reduce<TLeft, TRight>(this Either<TLeft, TRight> either, Func<TRight> whenLeft) =>
             either is Right<TLeft, TRight> right ? right.Content : whenLeft();
+
+        public static Either<TLeft, TNewRight> Map<TLeft, TRight, TNewRight>(this Either<TLeft, TRight> either, Func<TRight, TNewRight> map) =>
+            either is Right<TLeft, TRight> right
+                ? (Either<TLeft, TNewRight>)map(right.Content)
+                : (TLeft)(Left<TLeft, TRight>)either;
     }
 }
--- Functional/FunctionalTests/Either/ExtensionsTests.cs
+++ Functional/FunctionalTests/Either/ExtensionsTests.cs
@@ -45,5 +45,39 @@ namespace FunctionalTests.Either
 
             Assert.AreEqual("Replacement", result);
         }
+
+        [TestMethod]
+        public void MapOneTrackFunction_WhenRight_AppliesMap()
+        {
+            Either<int, string> either = "Hello World";
+
+            var result = either.Map(s => s + "!");
+
+            if (result is Right<int, string> right)
+            {
+                Assert.AreEqual("Hello World!", right.Content);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void MapOneTrackFunction_WhenLeft_DoesNotApplyMap()
+        {
+            Either<int, string> either = 4;
+
+            var result = either.Map(s => s + "!");
+
+            if (either is Left<int, string> left)
+            {
+                Assert.AreEqual(4, left.Content);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
     }
 }
