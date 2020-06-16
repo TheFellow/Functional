54ad1c1d2c5dab56e971c0c3d8df2d80bb9c6084
Ryan Harris <trharris@auw.com>
2020-06-15 17:26:21 -0700

    Added the Result type + some tests. Need to think about the extensions next.

---
+++ Functional/Functional/Results/Result.cs
@@ -0,0 +1,49 @@
+﻿using Functional.Option;
+using System;
+
+namespace Functional.Results
+{
+    public sealed class Result<T>
+    {
+        private readonly Option<T> _value;
+        private readonly string _message;
+
+        internal Result(T value, string message, bool success)
+        {
+            _value = success ? (Option<T>)new Some<T>(value) : None.Value;
+            _message = message;
+        }
+
+        public void OnOk(Action onOk)
+        {
+            if (_value is Some<T>)
+                onOk();
+        }
+        
+        public void OnOk(Action<T> onOk)
+        {
+            if (_value is Some<T> some)
+                onOk(some.Content);
+        }
+
+        public void OnFail(Action onFail)
+        {
+            if (_value is None<T>)
+                onFail();
+        }
+
+        public void OnFail(Action<string> onFail)
+        {
+            if (_value is None<T>)
+                onFail(_message);
+        }
+    }
+
+    public static class Result
+    {
+        public static Result<string> Ok() => new Result<string>("Ok", string.Empty, true);
+        public static Result<T> Ok<T>(T value) => new Result<T>(value, string.Empty, true);
+        public static Result<string> Fail() => new Result<string>(string.Empty, string.Empty, false);
+        public static Result<T> Fail<T>(string message) => new Result<T>(default!, message, false);
+    }
+}
---
+++ Functional/FunctionalTests/Results/ResultTests.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Functional.Results;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using System;
+
+namespace FunctionalTests.Results
+{
+    class MyClass { }
+
+    [TestClass]
+    public class ResultTests
+    {
+        private const string _failureMessage = "Test";
+
+        private bool _callbackInvoked = false;
+        private string _message = string.Empty;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _callbackInvoked = false;
+            _message = string.Empty;
+        }
+
+        private void SetCallbackInvoked(string message)
+        {
+            _callbackInvoked = true;
+            _message = message;
+        }
+
+        [TestMethod]
+        public void OnOk_WithOkResult_CallsAction()
+        {
+            var result = Result.Ok();
+
+            result.OnOk(SetCallbackInvoked);
+
+            Assert.IsTrue(_callbackInvoked);
+        }
+
+        [TestMethod]
+        public void OnOk_WithOkResult_PassesOriginalObjectToAction()
+        {
+            var myInstance = new MyClass();
+            MyClass? returnedInstance = null;
+            var result = Result.Ok(myInstance);
+
+            result.OnOk(mc => returnedInstance = mc);
+
+            Assert.IsNotNull(returnedInstance);
+            Assert.AreEqual(myInstance, returnedInstance);
+        }
+
+        [TestMethod]
+        public void OnOk_WithFailResult_DoesNotCallAction()
+        {
+            var result = Result.Fail();
+
+            result.OnOk(SetCallbackInvoked);
+
+            Assert.IsFalse(_callbackInvoked);
+        }
+
+        [TestMethod]
+        public void OnOk_WithFailResultAndMessage_DoesNotCallAction()
+        {
+            var result = Result.Fail<MyClass>(_failureMessage);
+            MyClass? myInstance = null;
+
+            result.OnOk(mc => myInstance = mc);
+
+            Assert.IsFalse(_callbackInvoked);
+            Assert.IsNull(myInstance);
+        }
+
+        [TestMethod]
+        public void OnFail_WithOkResult_DoesNotCallAction()
+        {
+            var result = Result.Ok();
+
+            result.OnFail(SetCallbackInvoked);
+
+            Assert.IsFalse(_callbackInvoked);
+        }
+
+        [TestMethod]
+        public void OnFail_WithOkResultAndPayload_DoesNotCallAction()
+        {
+            var result = Result.Ok(new MyClass());
+
+            result.OnFail(_ => throw new ArgumentException());
+
+            Assert.IsFalse(_callbackInvoked);
+        }
+
+        [TestMethod]
+        public void OnFail_WithFailResult_CallsAction()
+        {
+            var result = Result.Fail();
+
+            result.OnFail(SetCallbackInvoked);
+
+            Assert.IsTrue(_callbackInvoked);
+        }
+
+        [TestMethod]
+        public void OnFail_WithFailResult_CallsActionWithMessage()
+        {
+            var result = Result.Fail<MyClass>(_failureMessage);
+
+            result.OnFail(msg => SetCallbackInvoked(msg));
+
+            Assert.IsTrue(_callbackInvoked);
+            Assert.AreEqual(_failureMessage, _message);
+        }
+    }
+}
